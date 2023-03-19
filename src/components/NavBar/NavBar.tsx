a37529aabd793e07893be7be042796e46a8dae19
thecuong1396 <cuongphan@coinmap.tech>
2023-03-20 03:24:26 +0700

    handle css active button

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,25 +1,38 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import path from './../../constants/path'
+import classNames from 'classnames'
 
 interface Props {
   selectStatusMovie: (status: string) => void
 }
 export default function NavBar({ selectStatusMovie }: Props) {
   return (
-    <div>
-      <Link
+    <div className='flex flex-row justify-between gap-x-8'>
+      <NavLink
         to={path.now_playing}
         onClick={() => selectStatusMovie(path.now_playing)}
+        className={({ isActive }) =>
+          classNames('flex items-center capitalize  transition-colors', {
+            'text-skyblue': isActive,
+            'text-gray-600': !isActive
+          })
+        }
       >
         Now Playing
-      </Link>
-      <Link
+      </NavLink>
+      <NavLink
         to={path.top_rated}
         onClick={() => selectStatusMovie(path.top_rated)}
+        className={({ isActive }) =>
+          classNames('flex items-center capitalize  transition-colors', {
+            'text-skyblue': isActive,
+            'text-gray-600': !isActive
+          })
+        }
       >
         Top Rated movies
-      </Link>
+      </NavLink>
     </div>
   )
 }
